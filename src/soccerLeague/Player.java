730930f07f3e6b9eb618b491ef31e4dc2b2bb600
aldsan512 <aldsan_512@yahoo.com>
2016-03-26 18:48:56 -0500

    disregard this commit

---
+++ src/soccerLeague/Player.java
@@ -0,0 +1,12 @@
+package soccerLeague;
+
+public class Player extends RegisteredUser {
+
+	Player(int a, int h, String address, String name, String ph, String e) {
+		super(a, h, address, name, ph, e);
+	}
+
+
+
+
+}
