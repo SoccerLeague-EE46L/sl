73797ce7b0ddd0e52c0b77908db7fa4d8afd2778
Vishakh <Vishakh@Vishakh-THINK.attlocal.net>
2016-04-28 19:43:27 -0500

    dsf

--- SoccerLeague/src/soccerLeague/Team.java
+++ SoccerLeague/src/soccerLeague/Team.java
@@ -10,7 +10,7 @@ public abstract class Team {
 	public abstract void removePlayer(String email);
 	public abstract void removeCoach(String email);
 	
-	//"Hello Wodfgkrsffssld"
+	//"Hello Wodfgkrsfffsssld"
 
 }
 
